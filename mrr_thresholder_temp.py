bb5b8d4e3d80bd3c76b6bd36bbe3428da6c2cd60
GaTe92 <50341675+GaTe92@users.noreply.github.com>
2019-07-21 22:06:58 +0200

    Update mrr_thresholder_temp.py

--- mrr_thresholder_temp.py
+++ mrr_thresholder_temp.py
@@ -8,120 +8,84 @@ class MrrThreshold:
 
     def run(self, p_in=np.arange(0, 7, 0.02)):
 
-        # Demanding Parameters given by the Paper
-        # (THE DRxEAM: an integrated Photonic Thresholder)
+# -------------------------- Demanding Parameters given by the Paper (THE DRxEAM: an integrated Photonic Thresholder ----
 
-        A = 0.440  # Coupling ratio
-        r_1 = 0.980  # ratio1
-        r_2 = 0.972   # coupling ratio2#
-        psi_b = -0.0138   # additional phase bias#
-        phi_0 = -0.0894  # initial phaseshift#
-        lmda = 1500e-9  # free space wavelength#
-        R = 4e-6    # Radius of the MRR#
-        L = 2 * np.pi * R  # effective #
-        n_2 = 4.8e-17  # refractive index#
-        # n_1 = 3.45    # refractive index SOI strip waveguide platform#
-        a = np.sqrt(np.exp(-2.4 * L))  # round trip loss#
+A           =   0.440;                          ''' coupling ratio'''
+r_1         =   0.980;                          ''' coupling ratio1'''
+r_2         =   0.972;                          ''' coupling ratio2'''
+psi_b       =  -0.0628;                         ''' additional phase bias'''
+phi_0       =  -0.0894;                         ''' initial phaseshift'''
+lmda        =   1500e-9;                        ''' free space wavelength'''
+R           =   4e-6;                           ''' Radius of the MRR'''
+L           =   2 * np.pi * R;                  ''' effective '''
+n_2         =   4.2e-17;                        ''' refractive index'''
+n_1         =   2.45;                           ''' refractive index SOI strip waveguide platform'''
+a           =   np.sqrt(np.exp(-2.4 * L));      ''' round trip loss'''
 
-        # Input Power, discredited it certain steps#
-        # p_in = np.array([p_in])  # arange(0, 7, 0.02)
-        # Output Power in 3D for 3 possible stable solutions of phi
-        p_out = [.0, .0, .0]
-        # phase of Wave 1 determined by future calculation
-        phi_1 = [.0, .0, .0]
-        phi_2 = [.0, .0, .0]  # phase of Wave 2
+P_in        =   np.arange(0, 15, 0.02);         ''' Input Power, discredited it certain steps'''
+P_out       =   [.0];                           ''' Output Power in 3D for 3 possible stable solutions of phi'''
+phi_1       =   [.0, .0, .0];                   ''' phase of Wave 1'''          #determined by future calculation
+phi_2       =   [.0, .0, .0];                   ''' phase of Wave 2'''
 
-        A_eff = 1.2e-14
+A_eff       = 2e-14;
 
-        # Parameters depending on the MRR
-        # and coupling between MRR and MZI (WAVE 1)
+# -------------------------- parameters depending on the MRR and coupling between MRR and MZI (WAVE 1) -----------------
 
-        t_1 = [.0, .0, .0]  # complex transmission of MRR 1#
-        t_2 = [.0, .0, .0]  # complex transmission of MRR
+t_1         =   [.0, .0, .0];                  ''' complex transmission of MRR 1'''
+t_2         =   [.0, .0, .0];                  ''' complex transmission of MRR 2'''
 
-        # Lists to safe solution of the transcendental equation
+# --------------------------- buffer memory for parameters to solve transcendental equation of every wave --------------
 
-        phaseshift_1 = [.0, .0, .0]  # phase shift solutions 1#
-        phaseshift_2 = [.0, .0, .0]  # phase shift solutions
+coeff_1     =   [.0, .0, .0, .0]
+coeff_2     =   [.0, .0, .0, .0]
+# -------------------------- parameters depending on the MRR and coupling between MRR and MZI (WAVE 1) -----------------
 
-        # buffer memory for parameters to solve transcendental
-        # equation of every wave
+coeff_1[0]  = a * r_1
+coeff_1[1]  = -(a * r_1 * phi_0)
+coeff_1[2]  = (1 - a * r_1)**2
 
-        coeff_1 = [.0, .0, .0, .0]
-        coeff_2 = [.0, .0, .0, .0]
+# -------------------------- parameters depending on the MRR and coupling between MRR and MZI (WAVE 2) -----------------
 
-        # parameters depending on the MRR
-        # and coupling between MRR and MZI (WAVE 1)
+coeff_2[0]  = a * r_2;
+coeff_2[1]  = -(a * r_2 * phi_0);
+coeff_2[2]  = (1 - a * r_2)**2;
 
-        coeff_1[0] = a * r_1
-        coeff_1[1] = -(a * r_1 * phi_0)
-        coeff_1[2] = (1 - a * r_1)**2
+# -------------------------- just a bunch of variables i use as buffer memory ------------------------------------------# might be cumbersome but best way i could think of right now
 
-        # parameters depending on the MRR
-        # and coupling between MRR and MZI (WAVE 2)
+t           = [.0, .0, .0];
+i           = 0
+x           = [.0];
+phi_1_r     = .0;
+phi_2_r     = .0;
 
-        coeff_2[0] = a * r_2
-        coeff_2[1] = -(a * r_2 * phi_0)
-        coeff_2[2] = (1 - a * r_2)**2
+coeff_1[3] = -(1 - a * r_1) ** 2 * phi_0 - (
+                ((2 * np.pi * L * n_2) / (lmda * A)) * a ** 2 * (1 - r_2 ** 2) * (1- A) * P_in[i] / (A_eff * 1000));
 
-        # just a bunch of variables i use as buffer memory
-        # might be cumbersome but best way i could think of right now
+coeff_2[3] = (-(1 - a * r_2) ** 2 * phi_0 - ((2 * np.pi * L * n_2) / (lmda * A)) * a ** 2 * (
+                1 - r_1 ** 2) * A * P_in[i] / (A_eff * 1000));
 
-        # t = [.0, .0, .0]
-        i = 0
-        # x = [.0]
-        phi_1_r = 0
-        phi_2_r = 0
-        index2 = 0
+phi_1 = np.roots(coeff_1);
+phi_2 = np.roots(coeff_2);
 
-        while (i < len(p_in)):
-            coeff_1[3] = -(1 - a * r_1)**2 * phi_0 - (((2 * np.pi * L * n_2)
-                                                       / (lmda * A))
-                                                      * a ** 2 * (1 - r_2**2)
-                                                      * (1 - A) * p_in[i]
-                                                      / (A_eff * 1000))
-            coeff_2[3] = (-(1 - a * r_2)**2 * phi_0 - ((2 * np.pi * L * n_2)
-                                                       / (lmda * A))
-                          * a**2 * (1 - r_1 ** 2) * A * p_in[i]
-                          / (A_eff * 1000))
+for ii in range(0, 2, 1):
+    if(np.imag(phi_1[ii]) == 0):
+        phi_1_r = phi_1[ii]
 
-            phi_1 = np.roots(coeff_1)
-            phi_2 = np.roots(coeff_2)
-            index1 = 0
-            for ii in range(2, -1, -1):
-                if(np.imag(phi_1[ii]) == 0):
-                    phi_1_r = phi_1[ii]
-                    index1 += 1
-                if(np.imag(phi_2[ii]) == 0):
-                    phi_2_r = phi_1[ii]
-            if (index1 > 1):
-                index2 += 1
-            index1 = 0
+    if(np.imag(phi_2[ii]) == 0):
+        phi_2_r = phi_2[ii]
 
-            phaseshift_1.append(phi_1[ii])
-            phaseshift_2.append(phi_2[ii])
+    # to calculate t_1 from Equ. 6)
+    exp = np.exp(1j * (np.pi + phi_1_r))  # just to split up Equ. 5
+    t_1.append(exp * (a - (r_1 * np.exp(-1j * phi_1_r)) / (1 - a * r_1 * np.exp(1j * phi_1_r))));  # Equation 5 relation of energy
+    #t_1.append(t[i] * (phi_1_r * (A * P_in[i] / A_eff)));
+    # to calculate t_2 from Equ. 6)
+    exp = np.exp(1j * (np.pi + phi_2_r));  # same over here
+    t_2.append(exp * (a - r_2 * np.exp(-1j * phi_2_r)) / (1 - a * r_2 * np.exp(1j * phi_2_r)));
+    #t_2.append(t[i] * (phi_2_r * ((1 - A) * P_in[i] / A_eff)));
 
-            # to calculate t_1 from Equ. 6)
-            exp = np.exp(1j * (np.pi + phi_1_r))  # just to split up Equ. 5
-            t_1.append(exp * (a - (r_1 * np.exp(-1j * phi_1_r))
-                              / (1 - a * r_1 * np.exp(1j * phi_1_r))))
-            # Equation 5 relation of energy
-            # t_1.append(t[i] * (phi_1_r * (A * p_in[i] / A_eff)))
-
-            # to calculate t_2 from Equ. 6)
-            exp = np.exp(1j * (np.pi + phi_2_r))  # same over here
-            t_2.append(exp * (a - r_2 * np.exp(-1j * phi_2_r))
-                       / (1 - a * r_2 * np.exp(1j * phi_2_r)))
-            # t_2.append(t[i] * (phi_2_r * ((1 - A) * p_in[i] / A_eff)))
-
-            # all for sake to solve for p_out by Equ. 6)
-            # no reason that this is called >exp< i just need another variable
-            exp = np.exp(1j * psi_b)
-            exp = t_1[i] - exp * t_2[i]
-            p_out.append(A * (1 - A) * p_in[i] * np.absolute(exp)**2)
-
-            i += 1
-        plt.plot(p_out)
-        plt.show()
-        # print(index2)
-        return p_out
+    # all for sake to solve for P_out by Equ. 6)
+    exp = np.exp(1j * psi_b);  # no reason that this is called >exp< i just need another variable
+    exp = t_1[i] - exp * t_2[i];
+    P_out = (A * (1 - A) * P_in[i] * np.absolute(exp)**2);
+    
+return p_out
